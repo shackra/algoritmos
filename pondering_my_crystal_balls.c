d83890d077f657e27346df46edc90920595f93a5
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-07-02 18:34:56 -0600

    Implementa two crystal balls

---
+++ pondering_my_crystal_balls.c
@@ -0,0 +1,45 @@
+#include <math.h>
+#include <stdio.h>
+
+/*
+  Tenemos dos bolas de cristal, no importa cuantas veces se dejen
+  caer, no se rompen, sin embargo, pueden romperse a determinada
+  altura. Hay que usar ambas bolas de cristal para determinar esa
+  altura de manera eficiente.
+
+  expectativa: O(sqrt(n))
+  ver: https://frontendmasters.com/courses/algorithms/two-crystal-balls-problem/
+ */
+
+int find_optimal_height(const bool pisos[], size_t len) {
+  const int jmpBy = (int)floor(sqrt((double)len));
+
+  // NOLINTNEXTLINE
+  int i = jmpBy;
+  for (; i < len; i += jmpBy) {
+    if (pisos[i]) {
+      break;
+    }
+  }
+
+  // retrocedemos por la cantidad que saltamos
+  i -= jmpBy;
+
+  for (int j = 0; j < jmpBy && i < len; j++, i++) {
+    if (pisos[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+int main() {
+  bool pisos[] = {false, false, false, false, false,
+                  false, true,  true,  true,  true};
+  size_t len = sizeof(pisos) / sizeof(pisos[0]);
+
+  int best = find_optimal_height(pisos, len);
+  printf("la mejor altura en la que ambas bolas de cristal se rompe es %d\n",
+         best);
+}
